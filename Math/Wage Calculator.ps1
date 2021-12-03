deeba05c4993a5a493955d23a7aeeb68029eb275
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2021-12-03 11:12:47 -0800

    Update Wage Calculator.ps1
    Finished, works well except monthly calculations (due to the differing amounts of days in a month).

--- Math/Wage Calculator.ps1
+++ Math/Wage Calculator.ps1
@@ -1,12 +1,33 @@
+# Features to add:
+# Tax amount owed based by state and conditions
+
 Write-Host "Let's calculate your daily, weekly, monthly, and yearly wage."
 
-$hourly_pay= Read-Host -Prompt "What is your hourly pay (no money symbols please)"
-$hours= Read-Host -Prompt "How many hours do you work (per day, rounded)"
-$days= Read-Host -Prompt "How many days a week do you work"
+[decimal]$hourly_pay= Read-Host -Prompt "What is your hourly pay (no cash symbols)"
+[decimal]$hours= Read-Host -Prompt "How many hours do you work (per day, use decimal for minutes)"
+[int] $days= Read-Host -Prompt "How many days a week do you work"
+Write-Host
 
-$daily=$hourly_pay*$hours 
-# $weekly
-# $monthly
-# $yearly
+[decimal]$daily= $hourly_pay * $hours
+[decimal]$weekly= [int]$days * [decimal]$daily
+[int]$monthly= ($days * 4.3) * $daily
+[int]$yearly= ($days * 52) * $daily
 
-Write-Host "Daily pay: " $daily
+
+Write-Host "Daily pay: $" $daily
+Write-Host "$hourly_pay * $hours = $daily"
+Write-Host
+
+Write-Host "Weekly pay: $" $weekly
+Write-Host "$days * $daily = $weekly"
+Write-Host
+
+Write-Host "Monthly pay (slightly inaccurate): $" $monthly
+Write-Host "($days * 4.3) * $daily = $monthly"
+Write-Host
+
+Write-Host "Yearly/Annual pay: $" $yearly 
+Write-Host "($days * 52) * $daily = $yearly"
+Write-Host
+
+pause
