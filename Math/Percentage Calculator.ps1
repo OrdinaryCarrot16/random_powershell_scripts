b048c1849fb6aee7456bd6c3d71170ffcd6bbbda
Ozzy Smith <40549579+OrdinaryCarrot16@users.noreply.github.com>
2022-03-14 20:08:22 -0700

    Create Percentage Calculator.ps1

---
+++ Math/Percentage Calculator.ps1
@@ -0,0 +1,12 @@
+[decimal]$in_value= Read-Host -Prompt "Enter the number you want to find the percentage of (use period for decimal)"
+[decimal]$percent= Read-Host -Prompt "Enter the percentage you want to use (0-100, no symbols)"
+[decimal]$out_value= ($in_value * $percent) / 100
+[decimal]$percent_remaining=(100 - $percent)
+Write-Host
+
+Write-Host "Percentage: ($in_value * $percent) / 100 = $out_value"
+[decimal]$out_value= ($in_value * $percent_remaining) / 100
+Write-Host "Remaining Percentage (100 - $percent = $percent_remaining%): ($in_value * $percent_remaining) = $out_value"
+Write-Host
+
+pause
